a26fba5593bae373604661908c5e62867d107feb
Pedro J. Molina <pjmolina@metadev.pro>
2024-10-01 12:54:49 +0200

    ejemplo de ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,32 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { User } from './dominio/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app0';
+
+  lista: User[] = [
+    {
+      name: 'Jesica',
+      surname: 'Alba',
+    },
+    {
+      name: 'Pablo',
+      surname: 'Sanchez',
+    },
+    {
+      name: 'Lucia',
+      surname: 'Garcia',
+    },
+  ];
 
   selectedUser(user: User) {
     //console.log('Se seleciono ' + user.name + ' ' + user.surname);
